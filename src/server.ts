c555aa8dc307924fc30c92ea9b84bbf839ab6f51
Cesar <cesarochoa2006@github.com>
2022-01-03 08:55:07 -0500

    fix: added unstaged file

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import morgan from "morgan"
 export const app: Application = express()
 
 export const port: number | string = process.env.PORT ?? 5000
-app.use(morgan("common"))
+if (process.env.ENV === "production") app.use(morgan("common"))
 export const server = app.listen(port, () =>
   console.log(`Server is listening on port ${port}!`)
 )
